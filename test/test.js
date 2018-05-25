c3b6f0fcc26e0fde1cba8f11d70f37ce70c1ae07
Rémy MEJA <rmeja@defi-informatique.net>
2018-05-25 13:22:28 +0200

    Improve unit tests with some promises

--- test/test.js
+++ test/test.js
@@ -1,79 +1,54 @@
 'use strict';
 
-const pkg = require('../package.json'),
-  chai = require('chai'),
-  expect = chai.expect;
-
-const Promise = require('bluebird'),
-  cp = Promise.promisifyAll(require('child_process'));
+const chai = require('chai');
+const expect = chai.expect;
+const Promise = require('bluebird');
+const cp = Promise.promisifyAll(require('child_process'));
 
 it.optional = require('it-optional');
 
 // Dependencies tests
 describe('Check requirements', function () {
   this.timeout(15000);
-  it('Should be able to use sudo without error', function (done) {
-    cp.execAsync('sudo service --status-all').then((stdout, stderr) =>{
-      if(stdout && stdout.includes('[')){
-        done();
-        return;
-      }
-      done(new Error('Cannot access sudo or services, maybe your are not on debian/ubuntu'));
-    }).catch(error=>done(error));
-  })
+  it('Should be able to use sudo without error', function () {
+    return cp.execAsync('sudo service --status-all').then((stdout, stderr) => {
+      if (stdout && stdout.includes('[')) return Promise.resolve();
+      return Promise.reject(new Error('Cannot access sudo or services, maybe your are not on debian/ubuntu'));
+    });
+  });
 
-  it('Should have apache2 in the status list', function (done) {
-    cp.execAsync('sudo service --status-all').then((stdout, stderr) =>{
-      if(stdout && stdout.includes('apache2')){
-        done();
-        return;
-      }
-      done(new Error('Is Apache2 installed ?'));
-    }).catch(error=>done(error));
-  })
+  it('Should have apache2 in the status list', function () {
+    return cp.execAsync('sudo service --status-all').then((stdout, stderr) => {
+      if (stdout && stdout.includes('apache2')) return Promise.resolve();
+      return Promise.reject(new Error('Is Apache2 installed ?'));
+    });
+  });
 
-  it('Should not be able to access Apache2 because it does not exists dude ..', function (done) {
-    cp.execAsync('sudo service --status-all').then((stdout, stderr) =>{
-      if(stdout && stdout.includes('apache3')){
-        done(new Error('Apache3 is running.. the program have a big mistake'));
-        return;
-      }
-      done();
-    }).catch(error=>done(error));
-  })
-
-  // it.optional('Should have php running in service to be able to restart it (optional)', function (done) {
-  //   cp.execAsync('sudo service --status-all').then((stdout, stderr) =>{
-  //     if(stdout && stdout.includes('php')){
-  //       done();
-  //     }else{
-  //       done(new Error('Check if php in fpm or something like mod ?'));
-  //     }
-  //   }).catch(error=>done(error));
-  // })
-
+  it('Should not be able to access Apache2 because it does not exists dude ..', function () {
+    return cp.execAsync('sudo service --status-all').then((stdout, stderr) => {
+      if (stdout && stdout.includes('apache3')) return Promise.reject(new Error('Apache3 is running.. the program have a big mistake'));
+    });
+  });
 });
 
-describe('Check Class', function(){
+describe('Check Class', function () {
   this.timeout(15000);
-  it('Should restart apache2 if it is not started', function(done){
+  it('Should restart apache2 if it is not started', function () {
     const CheckingServices = require('../check.js');
     let check = new CheckingServices();
     // first stop apache
     cp.execSync('sudo service apache2 stop');
-    cp.exec('sudo service apache2 status', function(err, stdout, stderr){
+    return cp.execAsync('sudo service apache2 status', function (stdout, stderr) {
       expect(stdout).to.include('dead');
       expect(stdout).to.not.include('running');
       check.apache2();
+    }).delay(1000).then(() => {
       // Simulate an apache restart
-      setTimeout(function(){
-        cp.exec('sudo service apache2 status', function(err, stdout, stderr){
-          expect(stdout).to.include('running');
-          expect(stdout).to.not.include('dead');
-          check.apache2();
-          done();
-        });
-      },1000)
-    })
-  })
-})
+      return cp.execAsync('sudo service apache2 status', function (stdout, stderr) {
+        expect(stdout).to.include('running');
+        expect(stdout).to.not.include('dead');
+        check.apache2();
+      });
+    });
+  });
+});
